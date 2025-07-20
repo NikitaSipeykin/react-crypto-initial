c5349f443a7a238d8fbc82376a3ed62f521b6baa
MacNikSipeykin <niksipeykin@gmail.com>
2025-07-20 15:10:16 +0300

    object-map optimisation

--- src/components/layout/AppContent.jsx
+++ src/components/layout/AppContent.jsx
@@ -1,4 +1,5 @@
-import { Layout } from 'antd';
+import { Layout, Typography } from 'antd';
+import { useCrypto } from '../../context/crypto-context';
 
 const contentStyle = {
   textAlign: 'center',
@@ -8,7 +9,23 @@ const contentStyle = {
 };
 
 export default function AppSider() {
+  const { assets, crypto } = useCrypto();
+
+  const cryptoPriceMap = crypto.reduce((acc, coin) => {
+    acc[coin.id] = coin.price;
+    return acc;
+  }, {});
+
   return (
-    <Layout.Content style={contentStyle}>Content</Layout.Content>
+    <Layout.Content style={contentStyle}>
+      <Typography.Title level={3} style={{ textAlign: 'left', color: '#fff' }}>
+        Portfolio: {' '}
+        {assets
+          .map((asset) => asset.amount * cryptoPriceMap[asset.id])
+          .reduce((acc, v) => acc += v, 0)
+          .toFixed(2)}
+        $
+      </Typography.Title>
+    </Layout.Content>
   )
 }
